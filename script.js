177229ba5498aca54a59e853aa320a79dfc0ad5c
TTVTheAgregado <igorcauan036@gmail.com>
2023-12-02 02:54:17 -0400

    changing to my brand - Img

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ function toggleMode() {
   //substituir a img
   if (html.classList.contains("light")) {
     //se tiver o light mode,adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/igor.jpg")
   } else {
     //se tiver sem, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/igor.jpg")
   }
 }
